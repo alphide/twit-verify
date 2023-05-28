24c10f06e09fccdb8c1e0cec0ea1fd4338918e09
Jonathan C <jonathanchu71@gmail.com>
2023-05-28 05:43:26 -0400

    new running

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const user = new Twitter({
+  consumer_key: 'zlyG2FOaW4229TdqadJleKlxt',
+  consumer_secret: '0xahOMCJS9srUZZUqrqCu31Z72ot5BgYBPh6DLoQaS2qG8utx3',
+  access_token_key: '1542237757065633792-escQY4VCgckGXvLrsSkscFTjAtMlCj',
+  access_token_secret: 'H5LNqegV8Wcxsml6QsmbRRoPgzNQZpl2rh3Xz7dEqPCZS',
+});
 
 app.post('/', async (req, res) => {
     const username = req.body.username;
     const targetAccount = 'VirtuosoRBLX';
     console.log('Username:', username);
-    const followers = await reader.v2.followers(1542237757065633792)
+    const followers = await user.v2.followers(1542237757065633792)
 
     let next_token = followers.meta.next_token
     let flist = []
@@ -19,7 +25,7 @@ app.post('/', async (req, res) => {
 
     while(next_token !== undefined){
 
-        const more = await reader.v2.followers(userId, { asPaginator: true, pagination_token: next_token })
+        const more = await user.v2.followers(userId, { asPaginator: true, pagination_token: next_token })
         next_token =  more?.meta?.next_token
 
         more.data.data.map(e => flist.push(e.username))
