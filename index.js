a5e0ac4e76d333500eeb6177de6f3320df223316
Jonathan C <jonathanchu71@gmail.com>
2023-05-28 05:34:10 -0400

    new running

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const user = new Twitter({
-  consumer_key: 'zlyG2FOaW4229TdqadJleKlxt',
-  consumer_secret: '0xahOMCJS9srUZZUqrqCu31Z72ot5BgYBPh6DLoQaS2qG8utx3',
-  access_token_key: '1542237757065633792-escQY4VCgckGXvLrsSkscFTjAtMlCj',
-  access_token_secret: 'H5LNqegV8Wcxsml6QsmbRRoPgzNQZpl2rh3Xz7dEqPCZS',
-});
 
 app.post('/', async (req, res) => {
   try {
     const username = req.body.username;
     const targetAccount = 'VirtuosoRBLX';
     console.log('Username:', username);
-    const followers = [];
-    try {
-        followers = await user.get('followers/ids', { screen_name: targetAccount });
-    } catch (error) {
-        console.error('Error retrieving followers:', error);
-    }
+    const followers = await reader.v2.followers(1542237757065633792)
+
     console.log('VirtuosoRBLX:', followers);
     const isFollowing = followers.ids.includes(username);
   
