9511d902de85bb4746553c2525ac482444480da5
Jonathan C <jonathanchu71@gmail.com>
2023-05-28 03:17:19 -0400

    new require

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require "vendor/autoload.php";
-require "twitteroauth/autoload.php";
+require "vendor/abraham/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 $access = getenv('ACCESS');
